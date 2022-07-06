97b3eb0b091852eda7726f10931adf394104be51
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-06 17:18:29 +0530

    Updated files

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,22 @@
+import { Component, Fragment } from "react";
+import Greeting from "../../components/Home/Greeting";
+import Countable from "../../components/Home/Countable";
+import Divider from '@mui/material/Divider';
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Greeting name="GDSE"/>
+                <Divider light/>
+                <Countable />
+            </Fragment>
+        )
+    }
+}
+
+export default HomePage
