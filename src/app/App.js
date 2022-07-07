95ad4f3e6aea9516703f3938dbdcc1d96323de0a
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-07 07:57:42 +0530

    Updated file

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import HomePage from "../pages/Home"
+import HomePage from "../pages/Home";
 import Login from "../pages/session/Login";
+import {Routes, Route} from "react-router-dom";
+import NotFound from "../pages/session/NotFound";
+import FlexBoxLayout from "../pages/Layouts/FlexBox";
+import GridLayout from "../pages/Layouts/Gird"
 
 function App() {
   return (
-      <HomePage />
-     // <Login/>
+      <Routes>
+        <Route exact path='/' element={<HomePage/>}/>
+        <Route path='login' element={<Login/>}/>
+        <Route path='flex-layout' element={<FlexBoxLayout/>}/>
+        <Route path='grid-layout' element={<GridLayout/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+
   );
 }
 
