3adcccae42e49ffbdb07c568002f62324876dd1a
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-08 23:14:09 +0530

    updated item

--- src/pages/session/Item/index.jsx
+++ src/pages/session/Item/index.jsx
@@ -6,46 +6,83 @@ import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Autocomplete from '@mui/material/Autocomplete';
 import GDSEButton from "../../../components/common/Button"
-import {Link} from "react-router-dom";
 import NavBar from "../../../components/common/NavBar";
+import {Link, Route} from "react-router-dom";
 import Button from "@mui/material/Button";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+function createData(
+    code: string,
+    item: string,
+    price: string,
+    qty: number,
+    // protein: number,
+) {
+    return { code, item, price, qty};
+}
+
+const rows = [
+    createData('I-001', 'Rice', '700.00', 10),
+    createData('I-002', 'Toothpaste', '70.00', 5),
+    createData('I-003', 'Sugar', '600.00', 500),
+    createData('I-004', 'Soap', '135.00', 10),
+    // createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
 
 class Item extends Component {
+
+
+
+
+
     constructor(props) {
         super(props);
         this.state = {
             top100Films: [
                 { label: 'Rice' },
+                { label: 'Toothpaste' },
                 { label: 'Sugar' },
-                { label: 'Lux' },
-                { label: 'Salt' },
-                { label: 'Chillie' },
+                { label: 'Soap' }
+
             ]
         }
 
     }
 
     render() {
+
+
+
+
         const { classes } = this.props;
         return (
             <Fragment>
 
+
+
                 <div>
-                    <Link to="/" style={{position:'absolute', left:1100, textDecoration:"none" ,color:'black'}}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Home' wrapped={false} /></Link>
+                    <Link to="/" style={{position:'absolute', left:1100, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Home' wrapped={false} /></Link>
                     <Link to="customer" style={{position:'absolute', left:1200, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false} iconPosition='top' label='Customer' wrapped={false} /></Link>
                     <Link to="item" style={{position:'absolute', left:1300, textDecoration:"none" ,color:'black' }}><NavBar disabled={false} disableFocusRipple={false} disableRipple={false}  iconPosition='top' label='Item' wrapped={false} /></Link>
                 </div>
 
-
                 <div className={classes.title__container}>
-                <Typography variant="h4">
-                    Item Manage
-                </Typography>
+                    <Typography variant="h4">
+                        Item Manage
+                    </Typography>
                 </div>
 
                 <Grid container spacing={0.5}>
                     <Grid item lg={6} md={6} sm={6} xm={6} >
-                        <TextField id="outlined-basic" placeHolder="Code" label="Item Code" variant="outlined" size="small"
+                        <TextField id="outlined-basic" placeHolder="Code" label="Item code" variant="outlined" size="small"
                                    style={{width: '90%', top:150}} />
                     </Grid>
                     <Grid item lg={6} md={6} sm={6} xm={6}>
@@ -62,7 +99,7 @@ class Item extends Component {
                                 console.log(value.label + " " + value.year);
                             }}
                             size="small"
-                            style={{ width: '45%', top:150 , position:"absolute" }}
+                            style={{ width: '45%', top:150 , position:"absolute"}}
                         />
                     </Grid>
                     <Grid item lg={6} md={6} sm={6} xm={6} >
@@ -77,18 +114,55 @@ class Item extends Component {
                     <Grid item lg={12} md={12} sm={12} xm={12} style={{display: 'flex', position:"absolute", top:270, left:1000, width:300,color:"success"}} justifyContent="flex-end" >
                         <GDSEButton size="large" variant="contained" label="save"/>
 
-
                         <Button variant="contained" color="success" style={{left:50}}>
                             Update
                         </Button>
-                        <Button variant="outlined" color="error" style={{left:100}}>
+                        <Button variant="contained" color="error" style={{left:100}}>
                             Cancel
                         </Button>
+
+
+
                     </Grid>
-
-
-
                 </Grid>
+
+
+
+
+                <TableContainer component={Paper} style={{position:'absolute', top:380}} >
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Item Code</TableCell>
+                                <TableCell align="center">Item</TableCell>
+                                <TableCell align="center">Price</TableCell>
+                                <TableCell align="center">Qty</TableCell>
+                                {/*<TableCell align="right">Email</TableCell>*/}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.map((row) => (
+                                <TableRow
+                                    key={row.code}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {row.code}
+                                    </TableCell>
+                                    <TableCell align="center">{row.item}</TableCell>
+                                    <TableCell align="center">{row.price}</TableCell>
+                                    <TableCell align="center">{row.qty}</TableCell>
+                                    {/*<TableCell align="right">{row.email}</TableCell>*/}
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+
+
+
+
+
             </Fragment>
 
         )
